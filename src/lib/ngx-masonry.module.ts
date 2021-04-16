474e8a52f64bcec2c0d68a0766110771a6e00325
taliev <taliev@luxoft.com>
2021-04-17 01:23:55 +0300

    chore: remove not used import

--- src/lib/ngx-masonry.module.ts
+++ src/lib/ngx-masonry.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgxMasonryComponent } from './ngx-masonry.component';
 import { NgxMasonryDirective } from './ngx-masonry.directive';
 
